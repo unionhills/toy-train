079643a0cf0b841ec917966305156e9c43dbdf24
Union Hills Software <unionhills@cox.net>
2016-04-09 17:33:59 -0700

    Still tweaking the domain model.
    Added another domain object and heavily commented the existing domain
    objects.

---
+++ src/main/java/com/uhills/toytrain/domain/Property.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.uhills.toytrain.domain;
+
+/**
+ * Represents a structure or feature that can be
+ * attributed to a section of Terrain.
+ *
+ */
+public interface Property {
+
+}
